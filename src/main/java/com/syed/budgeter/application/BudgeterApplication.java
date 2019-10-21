f8cde7b0983ab71e88c386edfa3f3c27407dc3fc
Arsalan-Syed <arsalan.s96@gmail.com>
2019-10-21 22:12:37 +0100

    Initialized project with starter Spring web server

---
+++ src/main/java/com/syed/budgeter/application/BudgeterApplication.java
@@ -0,0 +1,12 @@
+package com.syed.budgeter.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgeterApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgeterApplication.class, args);
+    }
+}
